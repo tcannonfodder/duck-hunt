358ee0628a3e03f871371884f5bb6057b1e635cc
Thomas Cannon <tcannon00@gmail.com>
2020-06-23 15:22:46 -0400

    Add `DuckHuntTestCase`, adds `test` method from ActiveSupport
    * https://github.com/rails/rails/blob/8bec77cc0f1fd47677a331a64f68c5918efd2ca9/activesupport/lib/active_support/test_case.rb#L39

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -27,3 +27,18 @@ require 'duck-hunt'
 
 #require some basic test classes (useful for testing modules independently)
 require 'test_helper/test_classes'
+
+class DuckHuntTestCase < MiniTest::Test
+  def self.test(name, &block)
+    test_name = "test_#{name.gsub(/\s+/,'_')}".to_sym
+    defined = instance_method(test_name) rescue false
+    raise "#{test_name} is already defined in #{self}" if defined
+    if block_given?
+      define_method(test_name, &block)
+    else
+      define_method(test_name) do
+        flunk "No implementation provided for #{name}"
+      end
+    end
+  end
+end
